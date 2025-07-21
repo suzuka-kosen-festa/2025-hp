050439cb00d1fff685b503bc1007a8f6d380809a
shiron <shiron4710.dev@gmail.com>
2025-07-21 22:57:42 +0900

    style: lint

--- .github/scripts/lighthouse.js
+++ .github/scripts/lighthouse.js
@@ -11,13 +11,13 @@ function main(manifest) {
         const emoji = score >= 90 ? "🟢" : score >= 50 ? "🟠" : "🔴";
         return [key, `${emoji} ${score}`];
       });
-      const keys = kv.map((v) => v[0]);
-      const values = kv.map((v) => v[1]);
+      const keys = kv.map(v => v[0]);
+      const values = kv.map(v => v[1]);
       // to markdown table
-      const header = "|" + keys.join("|") + "|";
-      const row = "|" + values.join("|") + "|";
-      const output =
-        header + "\n" + "|" + kv.map(() => "---").join("|") + "|" + "\n" + row;
+      const header = `|${keys.join("|")}|`;
+      const row = `|${values.join("|")}|`;
+      const output
+        = `${header}\n` + `|${kv.map(() => "---").join("|")}|` + `\n${row}`;
       return output;
     })
     .join("\n\n");
