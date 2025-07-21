cca125e52757952d2d52adff089decb11edfb1ff
shiron <shiron4710.dev@gmail.com>
2025-07-22 01:08:56 +0900

    fix: vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,40 +1,28 @@
+import emotion from "@emotion/babel-plugin";
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from "@remix-run/dev";
 import react from "@vitejs/plugin-react";
-import typecript from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+declare module "@remix-run/cloudflare" {
+  interface Future {
+    v3_singleFetch: true
+  }
+}
 
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: "@emotion/react",
-    babel: {
-      plugins: [
-        [
-          "@emotion/babel-plugin",
-          {
-            importMap: {
-              "@mui/system": {
-                styled: {
-                  canonicalImport: ["@emotion/styled", "default"],
-                  styledBaseImport: ["@mui/system", "styled"],
-                },
-              },
-              "@mui/material/styles": {
-                styled: {
-                  canonicalImport: ["@emotion/styled", "default"],
-                  styledBaseImport: ["@mui/material/styles", "styled"],
-                },
-              },
-              "@mui/material": {
-                styled: {
-                  canonicalImport: ["@emotion/styled", "default"],
-                  styledBaseImport: ["@mui/material", "styled"],
-                },
-              },
-            },
-          },
-        ],
-      ],
-    },
-  }), typecript()],
+  plugins: [
+    remixCloudflareDevProxy(),
+    tsconfigPaths(),
+    react({
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
   test: {
     environment: "jsdom",
     globals: true,
