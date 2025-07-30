212a2cb8c5626277c68de89069e5f40866ef9500
kokokoko0825 <koushi.tanaka54@gmail.com>
2025-07-30 15:14:55 +0900

    feat: Stub

--- app/locales/ja.ts
+++ app/locales/ja.ts
@@ -1,11 +1,6 @@
 export const ja = {
   footer: {
     copyright: "©2025 鈴鹿工業高等専門学校 高専祭実行委員会",
-    fontSize: {
-      sectionTitle: "16px",
-      linkText: "14px",
-      copyright: "14px",
-    },
   },
   dummy: "hoge fuga piyo ダミー",
 };
