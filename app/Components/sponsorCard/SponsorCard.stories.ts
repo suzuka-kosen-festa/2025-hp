3b9cf58f4b1695cca448301581f78a1486dcfa8d
Ryoma Yoshitaka <75728941+tetra-mix@users.noreply.github.com>
2025-10-10 23:04:44 +0900

    feat: sponsor card (#199)
    * feat: sponsor card
    
    * fix: pnpm-lock.yaml error
    
    * restore: package.json pnpm-lock.yaml
    
    * fix: lint error
    
    * fix:locales file
    
    ---------
    
    Co-authored-by: Koshi <103725932+kokokoko0825@users.noreply.github.com>
    Co-authored-by: kokokoko0825 <koushi.tanaka54@gmail.com>

---
+++ app/Components/sponsorCard/SponsorCard.stories.ts
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import SponsorCard from "./SponsorCard";
+
+type T = typeof SponsorCard;
+
+const meta: Meta<T> = {
+  title: "SponsorCard",
+  component: SponsorCard,
+  parameters: {
+    layout: "centered",
+    viewport: {
+      // デフォルトをスマホ相当で確認できるように
+      defaultViewport: "responsive",
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    image: {
+      control: "text",
+      description: "スポンサー紹介画像URL",
+    },
+    sponsorName: {
+      control: "text",
+      description: "スポンサー名",
+    },
+    description: {
+      control: "text",
+      description: "スポンサーの説明（100文字程度）",
+    },
+    phone: {
+      control: "text",
+      description: "電話番号",
+    },
+  },
+};
+
+type Story = StoryObj<T>;
+
+const defaultArgs = {
+  image: "https://placehold.jp/800x400.png",
+  sponsorName: "テストスポンサー",
+  description:
+    "スポンサーの説明がここに入ります。スポンサーの説明がここに入ります。スポンサーの説明がここに入ります。スポンサーの説明がここに入ります。",
+  phone: "xxx-xxxx-xxxx",
+};
+
+export const Default: Story = {
+  args: defaultArgs,
+  parameters: {
+    viewport: {
+      defaultViewport: "desktop", // Storybook viewportアドオンで lg を確認
+    },
+  },
+};
+
+export default meta;
---
+++ app/Components/sponsorCard/SponsorCard.test.ts
@@ -0,0 +1,12 @@
+// @vitest-environment jsdom
+
+import { composeStories } from "@storybook/react";
+import { expect, it } from "vitest";
+import * as stories from "./SponsorCard.stories";
+
+const { Default } = composeStories(stories);
+
+it("sponsor card snapshot", async () => {
+  await Default.run();
+  expect(document.body.firstChild).toMatchSnapshot();
+});
