6139ab47dbf6a8ceceff5f506cc79190f2e6982d
kokokoko0825 <koushi.tanaka54@gmail.com>
2025-10-11 05:09:04 +0900

    feat: add eventCard

---
+++ app/Components/eventCard/EventCard.test.ts
@@ -0,0 +1,12 @@
+// @vitest-environment jsdom
+
+import { composeStories } from "@storybook/react";
+import { expect, it } from "vitest";
+import * as stories from "./EventCard.stories";
+
+const { Default } = composeStories(stories);
+
+it("eventCard snapshot", async () => {
+  await Default.run();
+  expect(document.body.firstChild).toMatchSnapshot();
+});
